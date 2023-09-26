f08d62abe57392ffd9f0d7e0a993e72d1fd62fed
MrSabin <sabin.21011986@gmail.com>
2023-09-27 00:23:19 +0300

    Authorisation page static and links

--- cuisine/urls.py
+++ cuisine/urls.py
@@ -3,5 +3,6 @@ from cuisine import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('auth', views.auth_user, name='auth')
+    path('auth', views.auth_user, name='auth'),
+    path('registration', views.register_user, name='registration'),
 ]
--- cuisine/views.py
+++ cuisine/views.py
@@ -7,3 +7,7 @@ def index(request):
 
 def auth_user(request):
     return render(request, 'auth.html')
+
+
+def register_user(request):
+    return render(request, 'registration.html')
